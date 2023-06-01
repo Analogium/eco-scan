3c5a07d18d28e7353742ecd25f3584d5884bed90
Icare741 <94448198+Icare741@users.noreply.github.com>
2023-06-01 12:37:19 +0200

    :sparkles: add scan component

--- eco-scan-app/src/app/core/core.module.ts
+++ eco-scan-app/src/app/core/core.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './components/nav/nav.component';
+import { ScanComponent } from './components/scan/scan.component';
 
 
 
 
 @NgModule({
   declarations: [
-    NavComponent
+    NavComponent,
+    ScanComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavComponent]
+    NavComponent,
+    ScanComponent]
 })
 export class CoreModule { }
