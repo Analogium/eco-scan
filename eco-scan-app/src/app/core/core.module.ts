8f5e2707e8299f6503dd80c2281b90fa2a836a82
Icare741 <94448198+Icare741@users.noreply.github.com>
2023-06-01 11:53:35 +0200

    :wrench: add arbo angular

---
+++ eco-scan-app/src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/nav/nav.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    NavComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
