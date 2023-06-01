14a6f37ff09812b254e9ac2824379ea5b070fc8e
Melaine Gérard <pro@melaine-gerard.fr>
2023-06-01 12:06:59 +0200

    :sparkles: Ajout crud produit

---
+++ eco-scan-api/migrations/Version20230601092301.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230601092301 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, barcode VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, electric_power DOUBLE PRECISION NOT NULL, electric_current DOUBLE PRECISION NOT NULL, electric_voltage DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE product');
+    }
+}
---
+++ eco-scan-api/src/Controller/ProductController.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\ProductRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Attributes as OA;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
+#[Route('/api/product', name: 'app_product_')]
+#[OA\Tag(name: 'Product')]
+class ProductController extends AbstractController
+{
+    public function __construct(
+        private readonly ProductRepository $productRepository,
+        private readonly EntityManagerInterface $entityManager
+    )
+    {}
+
+
+    #[Route('/', name: 'index', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(
+                ref: new Model(type: Product::class)
+            )
+        )
+    )]
+    public function index(): JsonResponse
+    {
+        $products = $this->productRepository->findAll();
+        return $this->json([
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new Model(
+            type: Product::class
+        )
+    )]
+    #[OA\Parameter(
+        name: "id",
+        in: 'path',
+        description: "Product id",
+        required: true,
+        schema: new OA\Schema(type: 'integer')
+    )]
+    public function show(int $id): JsonResponse
+    {
+        $product = $this->productRepository->find($id);
+        return $this->json([
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new Model(
+            type: Product::class
+        )
+    )]
+    #[OA\Parameter(
+        name: "id",
+        in: 'path',
+        description: "Product id",
+        required: true,
+        schema: new OA\Schema(type: 'integer')
+    )]
+    public function delete(int $id): JsonResponse
+    {
+        $product = $this->productRepository->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+        $this->entityManager->remove($product);
+        $this->entityManager->flush();
+        return $this->json([
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/', name: 'create', methods: ['POST'])]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new Model(
+            type: Product::class
+        )
+    )]
+    #[OA\RequestBody(
+        description: "Product object that needs to be added to the database",
+        required: true,
+        content: new Model(
+            type: Product::class,
+            groups: ['create']
+        )
+    )]
+    public function create(Request $request): JsonResponse {
+        $datas = json_decode($request->getContent(), true);
+
+        // validate datas
+        $errors = [];
+
+        if (!isset($datas['name'])) {
+            $errors['name'] = "Name is required";
+        }
+
+        if (!isset($datas['barcode'])) {
+            $errors['barcode'] = "Barcode is required";
+        }
+
+        if (!isset($datas['description'])) {
+            $errors['description'] = "Description is required";
+        }
+
+        if (!isset($datas['electricPower'])) {
+            $errors['electricPower'] = "Electric power is required";
+        }
+
+        if (!isset($datas['electricCurrent'])) {
+            $errors['electricCurrent'] = "Electric current is required";
+        }
+
+        if (!isset($datas['electricVoltage'])) {
+            $errors['electricVoltage'] = "Electric voltage is required";
+        }
+
+        if (count($errors) > 0) {
+            return $this->json([
+                'errors' => $errors
+            ], 422);
+        }
+
+
+        $product = new Product();
+
+        $product->setName($datas['name']);
+        $product->setBarcode($datas['barcode']);
+        $product->setDescription($datas['description']);
+        $product->setElectricPower($datas['electricPower']);
+        $product->setElectricCurrent($datas['electricCurrent']);
+        $product->setElectricVoltage($datas['electricVoltage']);
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        return $this->json([
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/{id}', name: 'update', methods: ['PUT'])]
+    #[OA\Response(
+        response: 200,
+        description: "Success",
+        content: new Model(
+            type: Product::class
+        )
+    )]
+    #[OA\Parameter(
+        name: "id",
+        in: 'path',
+        description: "Product id",
+        required: true,
+        schema: new OA\Schema(type: 'integer')
+    )]
+    #[OA\RequestBody(
+        description: "Product object that needs to be updated to the database",
+        required: true,
+        content: new Model(
+            type: Product::class,
+            groups: ['update']
+        )
+    )]
+    public function edit(int $id, Request $request): JsonResponse
+    {
+        $product = $this->productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found for id '.$id);
+        }
+
+        $datas = json_decode($request->getContent(), true);
+
+        $errors = [];
+
+        if (!isset($datas['name'])) {
+            $errors['name'] = "Name is required";
+        }
+
+        if (!isset($datas['barcode'])) {
+            $errors['barcode'] = "Barcode is required";
+        }
+
+        if (!isset($datas['description'])) {
+            $errors['description'] = "Description is required";
+        }
+
+        if (!isset($datas['electricPower'])) {
+            $errors['electricPower'] = "Electric power is required";
+        }
+
+        if (!isset($datas['electricCurrent'])) {
+            $errors['electricCurrent'] = "Electric current is required";
+        }
+
+        if (!isset($datas['electricVoltage'])) {
+            $errors['electricVoltage'] = "Electric voltage is required";
+        }
+
+        if (count($errors) > 0) {
+            return $this->json([
+                'errors' => $errors
+            ], 422);
+        }
+
+        $product->setName($datas['name']);
+        $product->setBarcode($datas['barcode']);
+        $product->setDescription($datas['description']);
+        $product->setElectricPower($datas['electricPower']);
+        $product->setElectricCurrent($datas['electricCurrent']);
+        $product->setElectricVoltage($datas['electricVoltage']);
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        return $this->json([
+            'product' => $product
+        ]);
+    }
+}
